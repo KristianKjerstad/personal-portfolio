f38bc2c9fce44a778531f4abd23f77515ca5b518
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2023-12-19 13:13:13 +0100

    add a section divider component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import { Header } from './components/Header'
 // import viteLogo from '/vite.svg'
 import "./App.css"
+import { SectionDivider } from './components/SectionDivider'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
     </h1>
     <h2 className="text-4xl pb-4 opacity-70">Web developer</h2>
     <button>Contact</button>
+    <SectionDivider title='About me' />
     </div>  )
 }
 
---
+++ src/components/SectionDivider.tsx
@@ -0,0 +1,13 @@
+import "../SectionDivider.css"
+
+
+
+export const SectionDivider = (props: {title: string}) => {
+    const {title} = props
+
+    return <div className="p-16 text-center text-3xl">
+        <h3>{title.toUpperCase()}</h3>
+        <div className="divider"></div>
+
+    </div>
+}
