d4d215f685a465f64a54bb93edee6ceab6277f9d
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-08-23 14:25:33 +0200

    more fix!

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/personal-portfolio/',
+    base: process.env.VITE_BASE_PATH || '/personal-portfolio/',
     css: {
         postcss: {
             plugins: [tailwindcss()],
